040c92f1fe52e220dc73afe506393aee587e8fb5
Subash Penneru <subashpenneru@gmail.com>
2018-08-17 12:23:06 +0530

    Revert "modified js file"
    This reverts commit 98a6f5e8d51b5e3c5da8ad456b4eb5c730ec8651.

--- js.js
+++ js.js
@@ -6,11 +6,8 @@
 
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Time and Date.</button>
-
 Click me to display Date and Time.</button>
-Click me to display</button>
-Click me to display Date</button>
+
 <p id="demo"></p>
 
 </body>
